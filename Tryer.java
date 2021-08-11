f904f57526725accb12bc4fcc6777b08b828c953
Kindeh Kuzekeseh Kum <74182189+EstyKim@users.noreply.github.com>
2021-08-11 04:55:31 +0200

    Add files via upload

---
+++ Tryer.java
@@ -0,0 +1,13 @@
+package average;
+
+public class Tryer {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		//Student.printStudentInfo();
+		//Student.addNewStudent();
+		//Student.findStudentWithHighestGrade();
+		//Student.findStudentById("7823401");
+		}
+}
